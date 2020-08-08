0d9825fe5b6d3da00ef607b8cd9899b057eed68c
DayanaBarquero <68917196+DayanaBarquero@users.noreply.github.com>
2020-08-07 23:17:38 -0600

    add

--- University.cpp
+++ University.cpp
@@ -5,52 +5,22 @@
 #include <sstream>
 #include "University.h"
 
-University(){}
-University::University(const string name)
-{
-    this->name=name;
-}
-University::University(const string name, const Professor* professor, const Administrative* admin)
-{
-    this->name=name;
-    this->professor=professor;
-    this->admin=admin;
+University::University() {}
 
-}
-string University::getName()
-{
-    return this->name;
+std::string University::getName(){return this->name;}
 
-}
-void University::setName(const string name)
-{
-    this->name=name;
+void University::setName(const std::string name){this->name=name;}
 
-}
-Professor* University::getProfessor()
-{
-    return this-> professor;
+Professor* University::getProfessor(){return this-> professor;}
 
-}
-void University::setProfessor(Professor* professor)
-{
-    this->professor=professor;
+void University::setProfessor(Professor* professor){this->professor=professor;}
 
-}
-Administrative* University::getAdministrative()
-{
-    return this->admin;
+Administrative* University::getAdministrative(){return this->admin;}
 
-}
-void University::setAdministrative(Administrative* admin)
-{
-    this->admin=admin;
+void University::setAdministrative(Administrative* admin){this->admin=admin;}
 
-}
 void University::addProfessor(Professor* professor)
 {
-
-
 }
 void University::getListProfessor()
 {
